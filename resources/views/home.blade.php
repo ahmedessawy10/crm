aa915a35b0ecd3bf615db39f18ddf889533f5f05
ahmedessawy10 <elessawy238@gmail.com>
2025-09-21 10:28:25 +0300

    edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -336,7 +336,7 @@
                     <div class="modal-dialog modal-dialog-centered modal-lg">
                         <div class="modal-content rounded-4 shadow">
                             <div class="modal-header">
-                                <h5 class="modal-title">{{ $item->title ?? '' }}</h5>
+                                <h5 class="modal-title"style="justify-content: space-between;">{{ $item->title ?? '' }}</h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
                             </div>
