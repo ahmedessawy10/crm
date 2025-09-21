c29b410742181f753215c3547fba3494f96af5be
ahmedessawy10 <elessawy238@gmail.com>
2025-09-21 14:28:10 +0300

    edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,7 +93,7 @@
             <!-- كارت 1 -->
 
             @php
-$colors = ['#823848', '#e7a74a', '#5a2c3c', '#014934', '#f39c12', '#16a085'];
+$colors = ['#ab7a59', '#094734de', '#ab7a59', '#014934', '#f39c12', '#16a085'];
 @endphp
             @foreach ($programs as $i=>$p )
             <div class="col-md-4" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="100">
